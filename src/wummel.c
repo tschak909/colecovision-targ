777d4cfb68ba62de68afd5de76b2e76e090128f2
Thomas <thom.cherryhomes@gmail.com>
2023-09-20 15:27:14 -0500

    wummel now on screen.

--- src/wummel.c
+++ src/wummel.c
@@ -9,15 +9,32 @@
 #include <os7.h>
 #include "patterns.h"
 
-SprStatus wummel_status;
-const SprFrame wummel_frame = {LIGHT_GREEN,0x50};
-const SprGraphics wummel_graphics = {SPRITE,0x50,wummel_left_pattern,1,wummel_frame};
-const SprObj wummel_obj = {wummel_graphics,wummel_status,0};
+/**
+ * @brief The reset (starting position for the wummel
+ */
+#define WUMMEL_RESET_X 220
+#define WUMMEL_RESET_Y 175
 
+static SprStatus wummel_status;
+static const SprFrame wummel_frame = {LIGHT_GREEN,0x00};
+static const SprGraphics wummel_graphics_left = {SPRITE,0x00,wummel_left_pattern,1,wummel_frame};
+static const SprGraphics wummel_graphics_right = {SPRITE,0x00,wummel_right_pattern,1,wummel_frame};
+static const SprGraphics wummel_graphics_up = {SPRITE,0x00,wummel_up_pattern,1,wummel_frame};
+static const SprGraphics wummel_graphics_down = {SPRITE,0x00,wummel_down_pattern,1,wummel_frame};
+static const SprObj wummel_obj_left = {wummel_graphics_left,wummel_status,0};
+static const SprObj wummel_obj_right = {wummel_graphics_right,wummel_status,0};
+static const SprObj wummel_obj_up = {wummel_graphics_up,wummel_status,0};
+static const SprObj wummel_obj_down = {wummel_graphics_down,wummel_status,0};
+
+void wummel_reset(void)
+{
+  wummel_status.x=WUMMEL_RESET_X;
+  wummel_status.y=WUMMEL_RESET_Y;
+}
 void wummel(void)
 {
-  wummel_status.x=232;
-  wummel_status.y=176;
-  activate(wummel_obj,false);
-  put_obj(wummel_obj,0);
+  wummel_status.x=220;
+  wummel_status.y=175;
+  activate(wummel_obj_left,false);
+  put_obj(wummel_obj_left,0);
 }
